6b72bd0a87b084fe90424f58dc6d4fef3c2808e8
Rhonda Eisenhauer <rhonda.s.eisenhauer@live.com>
2015-10-16 07:49:31 -0700

    first commit for cheers #1

--- Cheers/Program.cs
+++ Cheers/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,26 @@ namespace Cheers
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("Hello there, I'm only a week leate on this assignment.  The coding Gods hate me and have caused my computer to break. I will not be deterred.  What's your name?");
+            string name = Console.ReadLine();
+            System.Console.WriteLine("Hi " + name);
+            foreach (char letter in name.ToLower())
+
+            {
+                if (Char.IsLetter(letter))
+                {
+                    string mnemonic = "halfnorsemix";
+                    string article = "a";
+                    if (mnemonic.Contains(letter))
+
+                    {
+                        article = "an";
+                    }
+                        { System.Console.WriteLine("Give me " + article + "..." + letter);
+                        }
+                    }
+                }
+            }
         }
     }
-}
+    
